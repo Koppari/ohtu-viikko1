7ef7809b3186bab431b19d0055ed8808db7c1bc3
Koppari <robin.stachon@helsinki.fi>
2018-11-05 13:05:41 +0200

    t2

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -15,32 +15,32 @@ public class VarastoTest {
 
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
-		varasto = new Varasto(-1, -1);
-		assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+        varasto = new Varasto(-1, -1);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void konstruktoriLuoTyhjanVarastonTilavuudella0() {
-		varasto = new Varasto(-1);
-		assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+        varasto = new Varasto(-1);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void konstruktoriLuoVaraston() {
-		varasto = new Varasto(123, 123);
-		assertEquals(123, varasto.getSaldo(), vertailuTarkkuus);
+        varasto = new Varasto(123, 123);
+        assertEquals(123, varasto.getSaldo(), vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void konstruktoriAsettaaOikeanAlkusaldon() {
-		varasto = new Varasto(123, 124);
-		assertEquals(123, varasto.getSaldo(), vertailuTarkkuus);
+        varasto = new Varasto(123, 124);
+        assertEquals(123, varasto.getSaldo(), vertailuTarkkuus);
     }
 
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
         assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
-    }	
+    }
 
     @Test
     public void lisaysLisaaSaldoa() {
@@ -49,27 +49,27 @@ public class VarastoTest {
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
     }
 
-	@Test
+    @Test
     public void nollaLisaysEiLisaaSaldoa() {
         varasto.lisaaVarastoon(-1);
 
         assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void LiikaaLisaysEiLisaaSaldoa() {
         varasto.lisaaVarastoon(9999);
 
         assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
     }
-	
+
     @Test
     public void lisaysLisaaPienentaaVapaataTilaa() {
         varasto.lisaaVarastoon(8);
 
         assertEquals(2, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
-	
+
     @Test
     public void ottaminenPalauttaaOikeanMaaran() {
         varasto.lisaaVarastoon(8);
@@ -77,16 +77,16 @@ public class VarastoTest {
 
         assertEquals(2, saatuMaara, vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void nollaOttaminenPalauttaaOikeanMaaran() {
         varasto.lisaaVarastoon(8);
         double saatuMaara = varasto.otaVarastosta(-1);
 
         assertEquals(0, saatuMaara, vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void liikaaOttaminenPalauttaaOikeanMaaran() {
         varasto.lisaaVarastoon(8);
         double saatuMaara = varasto.otaVarastosta(99999);
@@ -98,14 +98,14 @@ public class VarastoTest {
     public void ottaminenLisääTilaa() {
         varasto.lisaaVarastoon(8);
         varasto.otaVarastosta(2);
-		
+
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
-	
-	@Test
+
+    @Test
     public void toStringOikein() {
-		String haluttu = varasto.toString();
+        String haluttu = varasto.toString();
         assertEquals(haluttu, varasto.toString());
     }
-	
+
 }
